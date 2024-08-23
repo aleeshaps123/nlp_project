7935d28da588ffaf70d7fa1541a53a3f392313b8
aleeshaps123 <143858482+aleeshaps123@users.noreply.github.com>
2024-08-23 11:12:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 # Load the summarization pipeline
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
 
 def summarize_text(text, max_length=130, min_length=30, do_sample=False):
